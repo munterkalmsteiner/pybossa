0c4964c3c13dc8205c9f5b31c9fa859810abe5bf
Michael Unterkalmsteiner <mun@bth.se>
2018-12-18 10:13:44 +0100

    Run also the background processes

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 source env/bin/activate
 redis-server &
 redis-server contrib/sentinel.conf --sentinel
+python app_context_rqworker.py scheduled_jobs super high medium low email maintenance &
 python run.py
