70787db52836a05e94cb2796cd80a13438eee789
Michael Unterkalmsteiner <mun@bth.se>
2019-01-15 17:32:22 +0100

    Start the scheduler also

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 source env/bin/activate
 redis-server &
 redis-server contrib/sentinel.conf --sentinel
+rqscheduler --host localhost &
 python app_context_rqworker.py scheduled_jobs super high medium low email maintenance &
 python run.py
